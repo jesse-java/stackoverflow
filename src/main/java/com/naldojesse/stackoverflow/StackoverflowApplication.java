b22e81067e7e59524849aecfb4af20e6e88649df
Jesse Naldo <naldojesse@jnaldoweb.com>
2018-03-07 23:27:16 -0800

    initial commit

---
+++ src/main/java/com/naldojesse/stackoverflow/StackoverflowApplication.java
@@ -0,0 +1,24 @@
+package com.naldojesse.stackoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+
+
+@Controller
+@SpringBootApplication
+public class StackoverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackoverflowApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String index()  {
+		return "index.jsp";
+	}
+
+}
